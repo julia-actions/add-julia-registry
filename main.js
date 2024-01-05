d43dab31442f416085d31ce39ec318ca7d2ddab6
Curtis Vogt <curtis.vogt@gmail.com>
2024-01-05 09:14:56 -0600

    Syntax refactor (#22)

--- main.js
+++ main.js
@@ -3,19 +3,16 @@ const os = require("os");
 const path = require("path");
 
 const tmp = require("tmp");
-const toml = require('toml');
+const toml = require("toml");
 
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 const io = require("@actions/io");
 
-const key = core.getInput("key", { required: true });
-const registry = core.getInput("registry", { required: true });
-
 const HOME = os.homedir();
 const DEPOT_PATH = (process.env.JULIA_DEPOT_PATH || path.join(HOME, ".julia")).split(path.delimiter);
 
-const startAgent = async () => {
+async function startAgent() {
   const { stdout } = await exec.getExecOutput("ssh-agent");
   stdout.split("\n").forEach(line => {
     const match = /(.*)=(.*?);/.exec(line);
@@ -23,22 +20,22 @@ const startAgent = async () => {
       core.exportVariable(match[1], match[2]);
     }
   });
-};
+}
 
-const addKey = async () => {
+async function addKey(key) {
   const { name } = tmp.fileSync();
   fs.writeFileSync(name, key.trim() + "\n");
   await exec.exec(`ssh-add ${name}`);
   await io.rmRF(name);
-};
+}
 
-const updateKnownHosts = async () => {
+async function updateKnownHosts() {
   const { stdout } = await exec.getExecOutput("ssh-keyscan github.com");
   await io.mkdirP(path.join(HOME, ".ssh"));
   fs.appendFileSync(path.join(HOME, ".ssh", "known_hosts"), stdout);
 }
 
-const cloneRegistry = async () => {
+async function cloneRegistry(registry) {
   const { name: tmpdir, removeCallback: tmpdirCleanup } = tmp.dirSync({ unsafeCleanup: true });
   await exec.exec(`git clone git@github.com:${registry}.git ${tmpdir}`);
   const meta = toml.parse(fs.readFileSync(path.join(tmpdir, "Registry.toml")));
@@ -56,17 +53,20 @@ const cloneRegistry = async () => {
   }
 };
 
-const configureGit = async () => {
+async function configureGit() {
   await exec.exec("git config --global url.git@github.com:.insteadOf https://github.com/");
-};
+}
 
-const main = async () => {
+async function main() {
+  const key = core.getInput("key", { required: true });
+  const registry = core.getInput("registry", { required: true });
+
   await startAgent();
-  await addKey();
+  await addKey(key);
   await updateKnownHosts();
-  await cloneRegistry();
+  await cloneRegistry(registry);
   await configureGit();
-};
+}
 
 if (!module.parent) {
   main().catch(e => {
--- post.js
+++ post.js
@@ -1,11 +1,11 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
 
-const undoGitConfig = async () => {
+async function undoGitConfig() {
   await exec.exec("git config --global --unset url.git@github.com:.insteadOf");
-};
+}
 
-const stopAgent = async () => {
+async function stopAgent() {
   const { stdout } = await exec.getExecOutput("ssh-agent -k");
   stdout.split("\n").forEach(line => {
     const match = /unset (.*);/.exec(line);
@@ -13,12 +13,12 @@ const stopAgent = async () => {
       core.exportVariable(match[1], "");
     }
   });
-};
+}
 
-const post = async () => {
+async function post() {
   await undoGitConfig();
   await stopAgent();
-};
+}
 
 if (!module.parent) {
   post().catch(e => {
